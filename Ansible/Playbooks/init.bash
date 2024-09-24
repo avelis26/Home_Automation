de6bba12eb67710d56f550901911e9bc9659f48c
Graham Pinkston <graham.pinkston@gmail.com>
2024-09-24 11:13:10 -0500

    flipped the safety on

--- Ansible/Playbooks/init.bash
+++ Ansible/Playbooks/init.bash
@@ -2,8 +2,8 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y git software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
-mkdir source
-cd source
+mkdir ~/source
+cd ~/source
 git clone git@github.com:avelis26/Home_Automation.git
-ansible-playbook core_setup.yml
-sudo reboot
+#ansible-playbook core_setup.yml
+#sudo reboot
