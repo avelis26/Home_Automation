604de6a62f1c907e07fbca1f5ffee782965da40f
Graham Pinkston <graham.pinkston@gmail.com>
2024-09-26 06:16:27 -0500

    testing in prod, making improvments

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -4,3 +4,12 @@ ssh-copy-id avelis@embyone
 cd ~/source/Home_Automation/Ansible/
 # MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
 ansible-playbook core_setup.yml
+
+
+    # VISUDO
+    - name: Update VISUDO
+      lineinfile:
+        path: /etc/sudoers.d/avelis
+        line: "avelis ALL=(ALL) NOPASSWD: ALL"
+        create: yes
+        mode: '0440'
