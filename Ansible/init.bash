e2bd8f4738bb1c2250bbf2f4f5b2b10c1074c3d1
Graham Pinkston <graham.pinkston@gmail.com>
2024-09-26 08:06:36 -0500

    updated bash removing junk

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -1,15 +1,5 @@
 # SSH to Ansible controller (pi-star)
 rm ~/.ssh/known_hosts
-ssh-copy-id avelis@embyone
 cd ~/source/Home_Automation/Ansible/
 # MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
-ansible-playbook core_setup.yml
-
-
-    # VISUDO
-    - name: Update VISUDO
-      lineinfile:
-        path: /etc/sudoers.d/avelis
-        line: "avelis ALL=(ALL) NOPASSWD: ALL"
-        create: yes
-        mode: '0440'
+git pull && git pull && clear && ansible-playbook core_setup.yml
