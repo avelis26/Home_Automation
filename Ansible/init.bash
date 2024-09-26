c301282c31d3eb385bc0339a502fd2544f7aae1d
Graham Pinkston <graham.pinkston@gmail.com>
2024-09-26 04:13:53 -0500

    updated init.bash with details for first time run

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -1,9 +1,6 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y git software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
-mkdir ~/source
-cd ~/source
-git clone git@github.com:avelis26/Home_Automation.git
-#ansible-playbook core_setup.yml
-#sudo reboot
+# SSH to Ansible controller (pi-star)
+rm ~/.ssh/known_hosts
+ssh-copy-id avelis@embyone
+cd ~/source/Home_Automation/Ansible/
+# MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
+ansible-playbook core_setup.yml
