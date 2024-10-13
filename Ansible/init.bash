b54a3111aa9dde170af96943800bf25448cbe098
Graham Pinkston <graham.pinkston@gmail.com>
2024-10-12 21:28:46 -0500

    cleaned up init.bash

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -1,5 +1,4 @@
 # SSH to Ansible controller (pi-star)
-rm ~/.ssh/known_hosts
 cd ~/source/Home_Automation/Ansible/
 # MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
-git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml
+git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml
