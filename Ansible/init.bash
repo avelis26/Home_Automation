48d299738b0555d6120d5fc354bcb95104b9c437
Graham Pinkston <graham.pinkston@gmail.com>
2025-01-23 13:49:29 -0600

    added embytwo

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -2,3 +2,4 @@
 cd ~/source/Home_Automation/Ansible/
 # MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
 git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml
+git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embytwo_setup.yml
