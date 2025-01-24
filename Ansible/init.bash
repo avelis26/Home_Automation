6bb578919668c181f1b3433a0652f2c285a508a0
Graham Pinkston <graham.pinkston@gmail.com>
2025-01-23 20:04:55 -0600

    added cd command

--- Ansible/init.bash
+++ Ansible/init.bash
@@ -1,5 +1,6 @@
 # SSH to Ansible controller (pi-star)
 cd ~/source/Home_Automation/Ansible/
 # MAKE SURE SECRETS FILES ARE UPDATED !!!!!!!
-git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml
-git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embytwo_setup.yml
+cd ~/source/Home_Automation/Ansible/ && git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml
+cd ~/source/Home_Automation/Ansible/ && git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embytwo_setup.yml
+cd ~/source/Home_Automation/Ansible/ && git status && git pull && git pull && rm ~/.ssh/known_hosts && clear && ansible-playbook embyone_setup.yml && ansible-playbook embytwo_setup.yml
