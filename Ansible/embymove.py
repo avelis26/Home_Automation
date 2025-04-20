93456eaa1d108512669aae65e43fdfd0140eb4a3
Graham Pinkston <graham.pinkston@gmail.com>
2025-04-20 15:00:31 -0500

    added exec comments

--- Ansible/embymove.py
+++ Ansible/embymove.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# ./embymove.py --user "grace" --server "embytwo" --input "/mnt/data/Torrents/Complete/embymove-text.txt" --output "/mnt/data/Media/Sports/NHL/"
+# ./embymove.py --user "grace" --server "embytwo" --input "/mnt/data/Torrents/Complete/NHL-2025-04-19.R1.G1_COL@DAL_TNT.mkv" --output "/mnt/data/Media/Sports/NHL/"
 
 import paramiko
 import hashlib
