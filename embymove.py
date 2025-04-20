5e2ebc58b2559adac0d3fc2895cefaacc6ea96bc
Graham Pinkston <graham.pinkston@gmail.com>
2025-04-20 15:28:09 -0500

    removed comment

--- embymove.py
+++ embymove.py
@@ -2,8 +2,6 @@
 # ./embymove.py --user "grace" --server "embytwo" --input "/mnt/data/Torrents/Complete/embymove-text.txt" --output "/mnt/data/Media/Sports/NHL/"
 # ./embymove.py --user "grace" --server "embytwo" --input "/mnt/data/Torrents/Complete/NHL-2025-04-19.R1.G1_COL@DAL_TNT.mkv" --output "/mnt/data/Media/Sports/NHL/"
 
-#!/usr/bin/env python3
-
 import paramiko
 import hashlib
 import os
